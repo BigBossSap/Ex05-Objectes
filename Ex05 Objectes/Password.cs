2231d2849b576a098230c9a5d5c185faff3c8fc7
SOC <SOC@IED310-PC10>
2023-01-25 17:34:34 +0100

    Agregar archivos de proyecto.

---
+++ Ex05 Objectes/Password.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05_Objectes
+{
+    class Password
+    {
+
+        int longitud;
+        string contraseña;
+
+        public Password()
+        {
+
+        }
+
+        public Password(int longitud)
+        {
+            this.Longitud = longitud;
+        }
+
+        public string Contraseña { get => contraseña; set => contraseña = value; }
+        public int Longitud { get => longitud; set => longitud = value; }
+
+        public string generarPassword()
+        {
+            string password="";
+
+            for(int i =0; i<Longitud; i++)
+            {
+                Random rnd = new Random();
+                Random rnd2 = new Random();
+
+               int num = rnd2.Next(0, 3);
+
+                if (num==0)
+                {
+                    password += Convert.ToChar(rnd.Next(48, 58));
+
+                }
+                else if (num==1)
+                {
+                    password += Convert.ToChar(rnd.Next(65, 91));
+
+                }
+
+                else if (num==2)
+                {
+                    password += Convert.ToChar(rnd.Next(97, 123));
+
+                }
+
+            }
+
+            Console.WriteLine(password);
+            return password;
+            
+        }
+
+        public bool esFuerte()
+        {
+            int mayusculas = 0;
+            int minusculas = 0;
+            int numeros = 0;
+            foreach (char c in Contraseña)
+            {
+                if (char.IsUpper(c))
+                {
+                    mayusculas++;
+                }
+                else if (char.IsLower(c))
+                {
+                    minusculas++;
+                }
+                else if (char.IsNumber(c))
+                {
+                    numeros++;
+                }
+            }
+            return mayusculas > 2 && minusculas > 1 && numeros > 5;
+        }
+    }
+
+  
+}
---
+++ Ex05 Objectes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex05_Objectes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Password password = new Password();
+            string contraseña = "ABCDEFG123dgf485";
+            password.Contraseña = contraseña;
+            password.Longitud = 8;
+
+            password.generarPassword();
+            Console.WriteLine(password.esFuerte());
+
+
+            Console.WriteLine("Longitud array: ");
+            int longitudArray = Convert.ToInt32(Console.ReadLine());
+
+            string[] passwords = new string[longitudArray];
+
+            for (int i = 0; i < passwords.Length; i++)
+            {
+
+                Password password[i] = new Password();
+                passwords[i]= 
+
+
+
+            }
+
+
+        }
+    }
+}
